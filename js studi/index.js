bf7313367188a158aa38f4c9cd2072aeb1d82730
EZRA GILANG <116784250+sodiumKlorida@users.noreply.github.com>
2025-05-23 16:49:51 +0700

    Add files via upload

---
+++ js studi/index.js
@@ -0,0 +1,9 @@
+function formRegist() {
+    const container = document.getElementById("form-container");
+    container.style.transform = "translateX(-50%)";
+}
+
+function formLogin() {
+    const container = document.getElementById("form-container");
+    container.style.transform = "translateX(0%)";
+}
